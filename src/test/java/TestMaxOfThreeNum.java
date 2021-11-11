3a3ed6eac1ea723a23a7649ce9f044c39e673f8e
Asus <dhanashreehakke7@gmail.com>
2021-11-11 23:12:40 +0530

    Added: Find Maximum number between three integer file is added

---
+++ src/test/java/TestMaxOfThreeNum.java
@@ -0,0 +1,21 @@
+import com.blp.generics.FindMaximum;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestMaxOfThreeNum {
+    @Test
+    public void checkMaximumNumber(){
+        int max = FindMaximum.getMaximum(50,20,5);
+        Assert.assertEquals(50,max);
+    }
+    @Test
+    public void checkMaximumNumber1(){
+        int max = FindMaximum.getMaximum(20,50,5);
+        Assert.assertEquals(50,max);
+    }
+    @Test
+    public void checkMaximumNumber2(){
+        int max = FindMaximum.getMaximum(20,5,50);
+        Assert.assertEquals(50,max);
+    }
+}
